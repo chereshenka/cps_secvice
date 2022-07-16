ecf30a83225d2ccdd01e28d46b6c42f32343e3a0
chereshenka <55566045+chereshenka@users.noreply.github.com>
2022-07-16 18:32:46 +0300

    with swiper

---
+++ swiper/app.js
@@ -0,0 +1,11 @@
+new Swiper('.swiper',{
+   Loop: true,
+   pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    slidesPerView: 1.4,
+   slidesOffsetBefore: 30,
+   spaceBetween: 16,
+});
