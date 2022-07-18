1f8480ca6268bbdf577d4bd19d9288f670fa0425
chereshenka <55566045+chereshenka@users.noreply.github.com>
2022-07-18 04:26:59 +0300

    rewrite all files

--- swiper/app.js
+++ swiper/app.js
@@ -1,11 +1,29 @@
-new Swiper('.swiper',{
-   Loop: true,
-   pagination: {
-      el: '.swiper-pagination',
-      type: 'bullets',
-      clickable: true,
-    },
-    slidesPerView: 1.4,
-   slidesOffsetBefore: 30,
-   spaceBetween: 16,
+let screen = window.matchMedia('(max-width: 767px)');
+
+window.addEventListener('resize', () => {
+   if (screen.matches) {
+      let newSwiper = new Swiper('.swiper',{
+         Loop: true,
+         pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+         },
+         slidesPerView: 1.4,
+         slidesOffsetBefore: 30,
+         spaceBetween: 16,
+      });
+   } else {
+      swiper.destroy();
+   }
+   
+});
+
+let showMoreButton = document.querySelector('.content__button');
+let list = document.querySelector('.swiper');
+
+showMoreButton.addEventListener('click', ()=>{
+   list.classList.toggle('hidden');
+   showMoreButton.classList.toggle('opened');
+   showMoreButton.textContent = !list.classList.contains('hidden')?'Скрыть':'Показать еще';
 });
